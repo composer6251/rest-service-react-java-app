98bfbc7ed7644d18dc4001d25310cace37ddd0ca
David Fennell <david.fennell@ruralsource.com>
2020-05-19 09:00:16 -0400

    Adding HelloWorldBean with path variable

--- src/main/java/com/training/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/training/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,12 @@
 package com.training.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloWorldController {
 
     @GetMapping("/helloworld")
@@ -13,6 +16,10 @@ public class HelloWorldController {
 
     @GetMapping("/helloworldbean")
     public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("Hello World Bean");
+        return new HelloWorldBean ("Hello World Bean");
+    }
+    @GetMapping("/helloworldbean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World %s", name));
     }
 }
