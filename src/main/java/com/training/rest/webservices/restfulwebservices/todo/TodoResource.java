df93ddda8c50bf0429eca19d84c992ca7daee71b
David <dfennell31@gmail.com>
2020-06-01 12:56:09 -0400

    Adding save method for front end PUt call

--- src/main/java/com/training/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/training/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,6 +1,8 @@
 package com.training.rest.webservices.restfulwebservices.todo;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,19 @@ public class TodoResource {
         return todoService.findAllTodos();
     }
 
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo getTodo(@PathVariable String username, @PathVariable long id){
+
+        return todoService.findById(id);
+    }
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo){
+        todoService.saveTodo(todo);
+
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+
+    }
     @DeleteMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
         Todo todo = todoService.deleteById(id);
@@ -28,4 +43,5 @@ public class TodoResource {
         }
         return ResponseEntity.notFound().build();
     }
+
 }
